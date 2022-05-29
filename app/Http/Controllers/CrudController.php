e514c6a15463fe5a378077605654e80a16942b7f
CoderHritik0 <hritiksanas47@gmail.com>
2022-05-29 17:34:25 +0530

    Form for Adding Data Created

---
+++ app/Http/Controllers/CrudController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\exam_subject;
+
+class CrudController extends Controller
+{
+    //
+    function addData(Request $req)
+    {
+            $exam_subject= new exam_subject;
+            $exam_subject->esm_id=$req->Exam_Subject_Marks_ID;
+            $exam_subject->exam_id=$req->Exam_ID;
+            $exam_subject->sds_id=$req->sds_ID;
+            $exam_subject->written_dateOfExam=$req->Date_Of_Written_Exam;
+            $exam_subject->written_totalMarks=$req->Total_Marks_of_Written_Exam;
+            $exam_subject->written_passingMarks=$req->Passing_Marks_of_Written_Exam;
+            $exam_subject->oral_dateOfExam=$req->Date_Of_Oral_Exam;
+            $exam_subject->oral_totalMarks=$req->Total_Marks_of_Oral_Exam;
+            $exam_subject->oral_passingMarks=$req->Passing_Marks_of_Oral_Exam;
+            $exam_subject->practical_dateOfExam=$req->Date_Of_Practical_Exam;
+            $exam_subject->practical_totalMarks=$req->Total_Marks_of_Practical_Exam;
+            $exam_subject->practical_passingMarks=$req->Passing_Marks_of_Practical_Exam;
+            $exam_subject->save();
+    }
+}
---
+++ resources/views/crud.blade.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>CRUD</title>
+
+        <!-- Fonts -->
+        <link
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"
+            rel="stylesheet"
+        />
+
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"
+        />
+
+        <link
+            rel="stylesheet"
+            href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}"
+        />
+
+        <link
+            rel="stylesheet"
+            href="{{ asset('dist/css/adminlte.min.css?v=3.2.0') }}"
+        />
+    </head>
+    <body clas>
+        <div class="card card-primary m-3 text-dark bg-light">
+            <div class="card-header">
+                <h3 class="card-title">CRUD Form for exam_subject table</h3>
+            </div>
+
+            <form class="form-horizontal" action="/Crud" method="POST">
+                @csrf
+                <div class="card-body">
+                    <div class="form-group row">
+                        <label for="inputEmail3" class="col-sm-3 col-form-label"
+                            >Exam_Subject_Marks ID</label
+                        >
+                        <div class="col-sm-9">
+                            <input
+                                type="number"
+                                min="0"
+                                name="Exam_Subject_Marks_ID"
+                                class="form-control"
+                                placeholder="Enter Exam_Subject_Marks ID"
+                                required
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label
+                            for="inputPassword3"
+                            class="col-sm-3 col-form-label"
+                            >Exam ID</label
+                        >
+                        <div class="col-sm-9">
+                            <input
+                                type="number"
+                                min="0"
+                                class="form-control"
+                                name="Exam_ID"
+                                placeholder="Exam ID"
+                                required
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label
+                            for="inputPassword3"
+                            class="col-sm-3 col-form-label"
+                            >sds ID</label
+                        >
+                        <div class="col-sm-9">
+                            <input
+                                type="number"
+                                min="0"
+                                class="form-control"
+                                name="sds_ID"
+                                placeholder="sds ID"
+                                required
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label
+                            for="inputPassword3"
+                            class="col-sm-3 col-form-label"
+                            >Date Of Written Exam</label
+                        >
+                        <div class="col-sm-9">
+                            <input
+                                type="date"
+                                class="form-control"
+                                name="Date_Of_Written_Exam"
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label
+                            for="inputPassword3"
+                            class="col-sm-3 col-form-label"
+                            >Total Marks of Written Exam</label
+                        >
+                        <div class="col-sm-9">
+                            <input
+                                type="number"
+                                min="0"
+                                max="9999"
+                                class="form-control"
+                                name="Total_Marks_of_Written_Exam"
+                                placeholder="Total Marks of Written Exam"
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label
+                            for="inputPassword3"
+                            class="col-sm-3 col-form-label"
+                            >Passing Marks of Written Exam</label
+                        >
+                        <div class="col-sm-9">
+                            <input
+                                type="number"
+                                min="0"
+                                max="9999"
+                                class="form-control"
+                                name="Passing_Marks_of_Written_Exam"
+                                placeholder="Passing Marks of Written Exam"
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label
+                            for="inputPassword3"
+                            class="col-sm-3 col-form-label"
+                            >Date Of Oral Exam</label
+                        >
+                        <div class="col-sm-9">
+                            <input
+                                type="date"
+                                class="form-control"
+                                name="Date_Of_Oral_Exam"
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label
+                            for="inputPassword3"
+                            class="col-sm-3 col-form-label"
+                            >Total Marks of Oral Exam</label
+                        >
+                        <div class="col-sm-9">
+                            <input
+                                type="number"
+                                min="0"
+                                max="9999"
+                                class="form-control"
+                                name="Total_Marks_of_Oral_Exam"
+                                placeholder="Total Marks of Oral Exam"
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label
+                            for="inputPassword3"
+                            class="col-sm-3 col-form-label"
+                            >Passing Marks of Oral Exam</label
+                        >
+                        <div class="col-sm-9">
+                            <input
+                                type="number"
+                                min="0"
+                                max="9999"
+                                class="form-control"
+                                name="Passing_Marks_of_Oral_Exam"
+                                placeholder="Passing Marks of Oral Exam"
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label
+                            for="inputPassword3"
+                            class="col-sm-3 col-form-label"
+                            >Date Of Practical Exam</label
+                        >
+                        <div class="col-sm-9">
+                            <input
+                                type="date"
+                                class="form-control"
+                                name="Date_Of_Practical_Exam"
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label
+                            for="inputPassword3"
+                            class="col-sm-3 col-form-label"
+                            >Total Marks of Practical Exam</label
+                        >
+                        <div class="col-sm-9">
+                            <input
+                                type="number"
+                                min="0"
+                                max="9999"
+                                class="form-control"
+                                name="Total_Marks_of_Practical_Exam"
+                                placeholder="Total Marks of Practical Exam"
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label
+                            for="inputPassword3"
+                            class="col-sm-3 col-form-label"
+                            >Passing Marks of Practical Exam</label
+                        >
+                        <div class="col-sm-9">
+                            <input
+                                type="number"
+                                min="0"
+                                max="9999"
+                                class="form-control"
+                                name="Passing_Marks_of_Practical_Exam"
+                                placeholder="Passing Marks of Practical Exam"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button type="submit" class="btn btn-outline-primary">
+                            Add Data
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
